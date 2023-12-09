87366e1298f096d512c3161c530e77a327f1db04
Aristóteles Coutinho <aristotelesbr@gmail.com>
2023-12-09 12:43:39 -0300

    feat: Add Configurable module and classes

---
+++ lib/configurable/config.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'configurable/option_builder'
+
+module Configurable
+  class Config
+    def initialize(builder: OptionBuilder.new)
+      @builder = builder
+      @options = {}
+    end
+
+    def on(env)
+      yield @builder if block_given?
+      @options[env] = @builder.build
+    end
+
+    def environment(env) = @options[env]
+  end
+
+  module Mixin
+    def self.included(klass)
+      klass.extend(ClassMethods)
+    end
+
+    module ClassMethods
+      def config
+        @config ||= Config.new
+      end
+    end
+  end
+end
---
+++ lib/configurable/option_factory.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'thread'
+
+module Configurable
+  module OptionFactory
+    @struct_cache = {}
+    @mutex = ::Mutex.new
+
+    def self.build(hash)
+      return unless hash.is_a?(::Hash)
+
+      struct_key = hash.keys.sort.join('_').to_sym
+      struct = get_or_create_struct(struct_key, hash.keys)
+
+      struct.new(hash)
+    end
+
+    def self.get_or_create_struct(struct_key, keys)
+      @mutex.synchronize do
+        @struct_cache[struct_key] ||= create_struct(keys)
+      end
+    end
+    private_class_method :get_or_create_struct
+
+    def self.create_struct(keys)
+      Struct.new(*keys, keyword_init: true) do
+        def initialize(hash)
+          hash.each do |k, v|
+            self[k] = v.is_a?(::Hash) ? OptionFactory.build(v) : v
+          end
+          freeze
+        end
+      end
+    end
+    private_class_method :create_struct
+  end
+end
